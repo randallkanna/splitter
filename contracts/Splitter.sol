1572c38ca0ef7c5c2521714ff944e75abc23f388
randallkanna <randallkanna@gmail.comm>
2018-05-07 21:00:40 -0700

    adds test framework

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -6,16 +6,19 @@ contract Splitter {
   address public splitTwo;
   address public owner;
 
-  constructor(address splitUser1, address splitUser2) public {
+  function Splitter(address splitUser1, address splitUser2) public {
     owner = msg.sender;
     splitOne = splitUser1;
     splitTwo = splitUser2;
   }
 
-  function split() public payable {
-    /* value = msg.value; */
+  function split() public payable returns (bool) {
+    require(splitOne != 0x0);
+    require(splitTwo != 0x0);
+    require(msg.sender.balance >= value);
+    uint256 value = msg.value;
 
-    /* splitOne.balance += msg.value / 2; */
-    /* splitTwo.balance += msg.value / 2; */
+    splitOne.transfer(value / 2);
+    splitTwo.transfer(value / 2);
   }
 }
---
+++ test/splitter.js
@@ -0,0 +1,15 @@
+// pragma solidity ^0.4.17;
+//
+// import "truffle/Splitter.sol";
+// import "truffle/DeployedAddresses.sol";
+// import "../contracts/Splitter.sol";
+//
+// contract TestSplitter {
+//   function testInitialBalance() {
+//     Splitter splitter = Splitter(DeployedAddresses.Splitter());
+//
+//     // uint expected = 10000;
+//
+//     // Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+//   }
+// }
