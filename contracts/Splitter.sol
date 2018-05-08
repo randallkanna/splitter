cfcff9a65f3302a38eb41286a071385741184708
randallkanna <randallkanna@gmail.comm>
2018-05-07 20:11:55 -0700

    adds split function

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,8 +2,20 @@ pragma solidity ^0.4.4;
 
 
 contract Splitter {
+  address public splitOne;
+  address public splitTwo;
+  address public owner;
 
-  function Splitter() {
-    // constructor
+  constructor(address splitUser1, address splitUser2) public {
+    owner = msg.sender;
+    splitOne = splitUser1;
+    splitTwo = splitUser2;
+  }
+
+  function split() public payable {
+    /* value = msg.value; */
+
+    /* splitOne.balance += msg.value / 2; */
+    /* splitTwo.balance += msg.value / 2; */
   }
 }
