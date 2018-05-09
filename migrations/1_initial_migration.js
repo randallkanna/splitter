6b5835a2a9b30617d8608a2f870a3968df5fe6d1
randallkanna <randallkanna@gmail.comm>
2018-05-08 18:28:41 -0700

    adds deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
+
+module.exports = function(deployer) {
+  deployer.deploy(Splitter)
+}
